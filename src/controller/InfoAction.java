317d3c40d3f028a423fcf7da79d6c9dcc4a9a9ac
cvele0 <vladancvjetkovic@gmail.com>
2021-10-20 20:14:52 +0200

    Popravljen toolbar, dodani paneli i menu linija

--- src/controller/InfoAction.java
+++ src/controller/InfoAction.java
@@ -7,7 +7,7 @@ import java.awt.event.KeyEvent;
 public class InfoAction extends AbstractRudokAction {
   public InfoAction() {
     putValue(ACCELERATOR_KEY, KeyStroke.getKeyStroke(KeyEvent.VK_I, ActionEvent.CTRL_MASK));
-    putValue(SMALL_ICON, loadIcon("images/info.png"));
+    putValue(SMALL_ICON, loadIcon("images/info25x25.png"));
     putValue(NAME, "Info");
     putValue(SHORT_DESCRIPTION, "Info");
   }
